dcfc5d941654a6d8551abb8b8b461b25c278645a
Germain Dudekem <germain.dudekem@infra.tigerspike.com>
2018-05-23 13:46:36 +1000

    Added functionality to display exisiting chatrooms and sending messages

--- jmsschat/chatroom.php
+++ jmsschat/chatroom.php
@@ -1,8 +1,27 @@
 <html>
 <head>
-    <title><?php echo $_GET["name"] ?>m</title>
+    <title><?php echo $_GET["name"] ?></title>
 </head>
 <body>
+<div>
+    <?php
+    $chatroomName = $_GET['name'];
+    $chatroomFile = fopen("chatrooms/$chatroomName.txt", 'r');
+
+    $chatroomContents = fread($chatroomFile, filesize("chatrooms/$chatroomName.txt"));
+    $chatroomArray = explode("\n", $chatroomContents);
+
+    foreach($chatroomArray as $line) {
+        if ($line != "") {
+            $lineArray = explode(",", $line);
+            $username = $lineArray[0];
+            $message = $lineArray[1];
+            echo "<div>$username: \t $message</div>";
+        }
+    }
+
+    ?>
+</div>
 <form action="sendmessage.php" method="post">
     <input type="text" name="message" placeholder="Enter message here">
     <input type="hidden" name="name" value="<?php echo $_GET["name"] ?>">
@@ -10,12 +29,3 @@
 </form>
 </body>
 </html>
-<?php
-/**
- * Created by PhpStorm.
- * User: work
- * Date: 2/5/18
- * Time: 3:04 PM
- */
-
-?>
--- jmsschat/sendmessage.php
+++ jmsschat/sendmessage.php
@@ -2,12 +2,13 @@
 // get username and message information
 $username = $_COOKIE["username"];
 $chatroomName = $_POST["name"];
-
-echo $_POST["name"];
+$message = $_POST["message"];
 
 // save information to chatroom file
-
+$chatroomFile = fopen("chatrooms/$chatroomName.txt", 'a');
+fwrite($chatroomFile, "$username,$message\n");
+fclose($chatroomFile);
 
 // return to chatroom page
-
+header("Location: http://jmsschat.konradangermains.space/chatroom.php?name=$chatroomName");
 ?>
